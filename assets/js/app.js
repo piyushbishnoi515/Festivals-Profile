bcf77329c28d78b581d210addc7d5f78348f0e3f
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-03-19 12:25:26 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const menuicon = document.querySelector(".three-lines");
+const list = document.querySelector(".nav-bar-top");
+
+menuicon.addEventListener("click", function () {
+    list.classList.toggle("menulist")
+})
